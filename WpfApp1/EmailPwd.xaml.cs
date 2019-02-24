cf6242045e47d5f037bdc581f64a98271eede8b6
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2019-02-24 20:48:01 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/EmailPwd.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SendMail
+{
+    /// <summary>
+    /// Логика взаимодействия для EmailPwd.xaml
+    /// </summary>
+    public partial class EmailPwd : Window
+    {
+        public string ToEmail
+        {
+            get { return (string)GetValue(ToEmailProperty); }
+            set { if (value != null) SetValue(ToEmailProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToEmailProperty =
+            DependencyProperty.Register(nameof(ToEmail),
+                typeof(string), typeof(EmailPwd),
+                new PropertyMetadata("")
+                //,new ValidateValueCallback(validateUserNameValue)
+                );
+
+
+        public EmailPwd()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void SaveCommand_Executed (object sender, ExecutedRoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace SendMail
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string WatchPath;
+        string toEmail;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Выберитe каталог для отслеживания",
+                EnsurePathExists = true,
+                ShowPlacesList = true
+            };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                WatchPath = dialog.FileName;
+            }
+            else
+            {
+                Close();
+            }
+
+            EmailPwd epwd = new EmailPwd();
+            if (epwd.ShowDialog() == true)
+            {
+                toEmail = epwd.ToEmail;
+            }
+            else Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
